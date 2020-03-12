043fe356f322a61bfa5f6ce0425a4f56c5c69daa
Riccardo Mollo <riccardo.mollo@securegroup.it>
2020-03-12 15:37:23 +0100

    added reader from file

--- pared.py
+++ pared.py
@@ -11,10 +11,10 @@
 #
 
 #### TODO:
-#### - output file
-#### - input file with many ip addresses
+#### - save out to output file
 #### - subnet instead of single ip
 #### - add http://ptrarchive.com/tools/lookup2.htm?ip=8.8.8.8
+#### - add https://api.hackertarget.com/reverseiplookup/?q=8.8.8.8
 
 import argparse
 import getopt
@@ -44,6 +44,33 @@ def logo():
     print(colored('Coded by: Riccardo Mollo', 'cyan'))
     print()
 
+def print_domains(ip):
+    try:
+        ip = str(ipaddress.ip_address(ip))
+    except ValueError:
+        print(colored('ERROR!', 'red', attrs = ['reverse', 'bold']) + ' IP address is not valid.')
+        sys.exit(1)
+
+    print('[+] IP: ' + colored(ip, 'white', attrs = ['bold']))
+
+    url = 'https://securitytrails.com/list/ip/' + ip
+    ua = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+
+    r = requests.get(url, headers = {'User-Agent': ua}, verify = False)
+    fqdns = re.findall('dns\"\>(.*?)\<\/a\>', r.text)
+    count = len(fqdns)
+
+    if count > 0:
+        n = colored(str(count), 'green')
+        print('[+] Found ' + n + ' domains:')
+
+        for fqdn in fqdns:
+            print(colored(fqdn, 'green'))
+    else:
+        print('[+] No domains found for IP ' + colored(ip, 'white', attrs = ['bold']) + ', sorry.')
+
+    print()
+
 def main(argv):
     parser = argparse.ArgumentParser(prog = 'pared.py')
     group = parser.add_mutually_exclusive_group(required = True)
@@ -54,32 +81,17 @@ def main(argv):
     ip = args.ip
     subnet = args.subnet
     file = args.file
-    ua = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 
     logo()
 
-    try:
-        ip = str(ipaddress.ip_address(ip))
-    except ValueError:
-        print(colored('ERROR!', 'red', attrs=['reverse', 'bold']) + ' IP address is not valid.')
-        sys.exit(1)
-
-    print('[+] IP: ' + colored(ip, 'white', attrs=['bold']))
-
-    url = 'https://securitytrails.com/list/ip/' + ip
-
-    r = requests.get(url, headers={'User-Agent': ua}, verify = False)
-    fqdns = re.findall('dns\"\>(.*?)\<\/a\>', r.text)
-    count = len(fqdns)
-
-    if count > 0:
-        n = colored(str(count), 'green')
-        print('[+] Found ' + n + ' domains:')
-
-        for fqdn in fqdns:
-            print('    ' + colored(fqdn, 'green'))
-    else:
-        print('[+] No domains found, sorry.')
+    if ip is not None:
+        print_domains(ip)
+    elif subnet is not None:
+        print("TODO")
+    elif file is not None:
+        with open(file) as reader:
+            for line in reader:
+                print_domains(line.strip())
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
